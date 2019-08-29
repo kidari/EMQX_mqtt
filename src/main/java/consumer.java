b7a8d28509983ba8475bc88de9c6dd7ffebc69c1
kidari <kidari@qq.com>
2019-08-29 10:56:50 +0800

    Initial commit

---
+++ src/main/java/consumer.java
@@ -0,0 +1,27 @@
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+public class consumer {
+    public static void main(String[] args) throws Exception {
+//        String host = "tcp://172.16.40.173:1883";
+        String host = "tcp://119.3.210.123:1883";
+        String topic = "lwm2m/ep1/up/resp";
+//        String topic = "lwm2m/866971030604891/up/resp";
+//        String topic = "testqwer";
+        String clientId = "123456";// clientId不能重复
+        // 1.设置mqtt连接属性
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(true);
+        // 2.实例化mqtt客户端
+        MqttClient client = new MqttClient(host, clientId);
+        // 3.连接
+        client.connect(options);
+
+        client.setCallback(new PushCallback());
+        while (true) {
+            client.subscribe(topic, 2);
+        }
+        // client.disconnect();
+    }
+
+}
---
+++ src/main/java/server.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class server {
+    public static void main(String[] args) throws Exception {
+//        String host = "tcp://172.16.40.173:1883";
+        String host = "tcp://119.3.210.123:1883";
+        String topic = "lwm2m/866971033461919/dn";
+//        String topic = "testtopic";
+        String clientId = "server";// clientId不能重复
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(true);
+
+        MqttClient client = new MqttClient(host, clientId);
+        client.connect(options);
+
+        MqttMessage message = new MqttMessage();
+
+//        @SuppressWarnings("resource")
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("请输入要发送的内容：");
+//        while (true) {
+//            String line = scanner.nextLine();
+            String line = "{\n" +
+                    "    \"reqID\": \"2\",\n" +
+                    "    \"msgType\": \"discover\",\n" +
+                    "    \"data\": {\n" +
+                    "        \"path\": \"/3/0\"\n" +
+                    "    }\n" +
+                    "}";
+            message.setPayload(line.getBytes());
+            client.publish(topic, message);
+            client.close();
+
+//        }
+    }
+}
